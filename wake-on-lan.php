80d4886e63b5430025a1dc96dfdb841b405b9bc4
Andreas Schaefer <AndiSHFR@users.noreply.github.com>
2021-11-04 20:39:51 +0100

    Error fixing and code improvements.
    - Updating the reachability of table entries was buggy.
    - When hovering with the mouse over the "reachability state" there will be a popup telling that tcp port 3389 will be tested. 
    - When writing to the socket fails there will be an error message.
    - Moved code from mini-i18n.js into the wake-on-lan.php. So it is really a one file solution as stated in the README.
    - Checking reachability with an empty ip address (query params like: "&op=info&ip=") returned page html instead json result.

--- wake-on-lan.php
+++ wake-on-lan.php
@@ -1,4 +1,4 @@
-<?php
+<?php //
 /* 
  * PHPWOL - Send wake on lan magic packet from php.
  * PHP Version 5.6.28
@@ -48,6 +48,8 @@ function wakeOnLan($mac, $ip, $cidr, $port, &$debugOut) {
 	if(!$wolResult) {
 		$debug[] = __LINE__ . ' : Validating mac address: ' . $mac; 
 		$mac = str_replace(':','-',strtoupper($mac));
+		$debugOut[] = __LINE__ . ' : MAC = ' . $mac;
+
 		if ((!preg_match("/([A-F0-9]{2}[-]){5}([0-9A-F]){2}/",$mac)) || (strlen($mac) != 17)) {
 			$wolResult = 'Error: Invalid MAC-address: ' . $mac;
 			$debugOut[] = __LINE__ . ' : ' . $wolResult;
@@ -125,12 +127,13 @@ function wakeOnLan($mac, $ip, $cidr, $port, &$debugOut) {
 		} else {
 			$debugOut[] = __LINE__ . ' : Sending magic paket with ' . strlen($magicPacket) . ' bytes using fwrite().';														
 			// Send the magic packet
-			$writeResult = fwrite($socket, $magicPacket);
+			$writeResult = @fwrite($socket, $magicPacket);
 			if(!$writeResult) {
-				$wolResult = 'Error: ' . $errNo . ' - ' . $errStr;
+				$lastError = error_get_last();
+				$wolResult = 'Error: ' . $lastError['message'];
 				$debugOut[] = __LINE__ . ' : ' . $wolResult;												
 			}	else {
-				$debugOut[] = __LINE__ . ' : Magic packet has been send to address ' . $ip;
+				$debugOut[] = __LINE__ . ' : Magic packet has been sent to address ' . $ip;
 			}			
 			// Clean up the socket
 			fclose($socket);
@@ -188,6 +191,28 @@ function wakeOnLan($mac, $ip, $cidr, $port, &$debugOut) {
   return $wolResult;
 }
 
+function sendIcmpEchoRequest($host, $timeout = 1) {
+	/* ICMP ping packet with a pre-calculated checksum */
+	$package = "\x08\x00\x19\x2f\x00\x00\x00\x00\x70\x69\x6e\x67";
+	$socket  = socket_create(AF_INET, SOCK_RAW, 1);
+	socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => $timeout, 'usec' => 0));
+	socket_connect($socket, $host, null);
+	$ts = microtime(true);
+	socket_send($socket, $package, strLen($package), 0);
+	if (socket_read($socket, 255)) {
+			$result = microtime(true) - $ts;
+	} else {
+			$result = false;
+	}
+	socket_close($socket);
+	return $result;
+}
+
+function endWithErrorMessage($message) {
+  http_response_code(500);
+	die('Internal Server Error! ' . $message);
+}
+
 function endWithJsonResponse($responseData) {
 
 	array_walk_recursive($responseData, function(&$value, &$key) {
@@ -213,6 +238,7 @@ function endWithJsonResponse($responseData) {
 
 
 
+
 // Init locale variables
 $MESSAGE = false;     // false -> all is fine, string -> error message
 $DEBUGINFO = [];         // Array of strings containing debug information
@@ -229,8 +255,8 @@ $COMMENT     = isset($_GET['comment']) ? $_GET['comment'] : '';
 
 
 // Is it a "Get host state" request?
-if('info'===$OP && '' != $IP) {
-
+if('info'==$OP) {	
+ if(''==$IP) endWithErrorMessage('Parameter IP not set.');
  $responseData = [ 'error' => false, 'isUp' => false ];
 
  $errStr = false;
@@ -498,7 +524,196 @@ if(!$MESSAGE) $MESSAGE = '';
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"  crossorigin="anonymous"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha256-KM512VNnjElC30ehFwehXjx1YCHPiQkOPmqnrWtpccM=" crossorigin="anonymous"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <script src="mini-i18n.js"></script>
+  
+<script>
++function(window, $, undefined) {
+    'use strict';
+  
+    // PRIVATE
+  
+        // Help overcome console issues under IE when dev tools are not enabled but debug is set to true.
+    var console = (window.console = window.console || {}),
+  
+        // Global options for this module
+        options = {
+          // True will output debug information on the developer console
+          debug: false,
+          // css style to be applied to the element if the language text for the key was not found
+          notFound: 'lang-not-found',
+          // If set this points to a RESTful api to GET the language text
+          api: undefined,
+          // User callback to be called _before_ a text is assigned to an element.
+          // If the callback returns true the default behaviour will not be executed.
+          onItem: undefined,
+          // Data cache for already loaded/used languages
+          data: []
+        },
+  
+        /**
+         * Output debug information to the developer console
+         *
+         * @param {object} args_
+         * @return
+         * @api private
+         */
+        debug = function(args_) {
+          if(options.debug) {
+            var args = [].slice.call(arguments);
+            args.unshift('mini-i18n: ');
+            console.log.apply(null, args);
+          }
+        },  
+  
+        /**
+         * Get a value from an object by its path
+         *
+         * @param {object} obj
+         * @param {string} path
+         * @return {object}
+         * @api private
+         */
+        deepValue = function(obj, path) {
+          if('string' !== typeof path) return obj;
+          path = path.replace(/\[(\w+)\]/g, '.$1');   // convert indexes to properties
+          path = path.replace(/^\./, '').split('.');  // strip a leading dot and split at dots
+          var i = 0, len = path.length;               
+          while(obj && i < len) {
+            obj = obj[path[i++]];
+          }
+          return obj;
+        },
+  
+        /**
+         * Loops thru all language elements and sets the current language text
+         * 
+         * @param {string} lang
+         * @return
+         * @api private
+         */
+        updateElements = function(lang) {
+          debug('Updating elements with language: ' + lang);
+          var data = options.data[lang],  // data containing language related text. 
+              // Callback called for every item to allow custom handling
+              // If the callback returns false the default handling take place
+              cb = options.onItem || function() { return false; }
+              ;
+  
+          // Select all elements and loop thru them
+            $('[data-lang-ckey],[data-lang-tkey],[data-lang-pkey]').each(function () {
+              debug('Updating:', this); 
+            var $this = $(this),                 // jQuery object of the element            
+                ckey = $this.attr('data-lang-ckey'), // Key for the content of the element
+                tkey = $this.attr('data-lang-tkey'), // Key for title attribute of the element
+                pkey = $this.attr('data-lang-pkey'), // Key for placeholder attribute of the element
+                cval = deepValue(data, ckey),      // Value of the content
+                tval = deepValue(data, tkey),      // Value of the title attribute
+                pval = deepValue(data, pkey)       // Value of the placeholder attribute
+                ;
+  
+            // Execute callback and if the result is false run the default action
+            if(false === cb(this, lang, ckey, cval, tkey, tval, pkey, pval)) {
+              // If there is a content key set the content and handle "not found" condition
+              if(ckey) {
+                $this
+                .removeClass(options.notFound)
+                .html(cval)
+                .addClass( (cval ? undefined : options.notFound ) );
+              }          
+              // If there is a title key set the title attribute and handle "not found" condition
+              if(tkey) {
+                $this
+                .removeClass(options.notFound)
+                .attr('title', (tval || $this.attr('title')) )
+                .addClass( (tval ? undefined : options.notFound ) );
+              }
+              // If there is a placeholder key set the placeholder attribute and handle "not found" condition
+              if(pkey) {
+                $this
+                .removeClass(options.notFound)
+                .attr('placeholder', (pval || $this.attr('placeholder')) )
+                .addClass( (pval ? undefined : options.notFound ) );
+              }
+            }
+          });
+        },
+        
+        /**
+         * Sets configuration values 
+         * 
+         * @param {object} options_
+         * @return
+         * @api private
+         */
+        configure = function(options_) {
+          debug('configure with: ', options_);
+          options = $.extend({}, options, options_);
+        },
+  
+        /**
+         * Switch language text on elements
+         * 
+         * @param {string} lang
+         * @return
+         * @api private
+         */
+        language = function(lang) {        
+          debug('Switch to language: ', lang);
+          if(!options.data[lang] && options.url) {
+            var url = options.url + lang;
+            debug('Requesting language data for "' + lang + '" from url "' + url + '"');          
+            $.ajax({
+              url: url,
+             success: function(res) { 
+               debug('Got response.', res);          
+               options.data[lang] = res;
+             },
+             complete: function(res) { 
+               if(!options.data[lang]) {
+                 debug('Got no valid response. Language "' + lang + '" will not be available!');          
+                 options.data[lang] = {}; // Create an empty object so further requests won't lead to ajax calls anymore.
+               }
+               updateElements(lang);
+              }
+            });
+          } else {
+            updateElements(lang);
+          }
+        }
+        ;
+  
+    // PUBLIC
+  
+    /**
+     * Public mini-i18n method.
+     * Can be called in two ways.
+     * Setting options    : p is an object with configuration settings.
+     * Switching language : p is a string with the language name. i.e. 'en-US'
+     * 
+     * @param {object|string} p 
+     * @return
+     * @api public
+     */
+    $.fn.extend({
+      miniI18n : function(p) {
+        if('string' === typeof p) return language(p);
+        if('object' === typeof p) return configure(p);
+        throw new Error('Argument must be a string or an object with configuration values.');
+      }
+    });
+  
+  
+    $(function() {
+      // Auto initialize elements with attribute "data-lang-switch"
+      $('[data-lang-switch]').on('click.mini-i18n', function(e) {
+        e.preventDefault();
+        var lang = $(this).attr('data-lang-switch');
+        if(lang) $.fn.miniI18n(lang);
+      });
+    });
+  
+  }(window, jQuery);
+
+</script>
 
 <script>
 $(function () { 'use strict'
@@ -509,6 +724,8 @@ $(function () { 'use strict'
 		* This leads to an exception when using console.log(...); w/o dev tools opened.
 		*/
 	console = (window.console = window.console || { log: function() {} });
+
+	var debug = <?php echo intval($ENABLEDEBUG); ?>;
 
   function pageNotify(msg, style, dismissable, autoClose) {
     if(!msg || '' === msg) { $('.pageNotifications').empty(); return };
@@ -535,7 +752,7 @@ $(function () { 'use strict'
   function addItemToTable(mac, ip, cidr, port, comment) {
 		var $item = $([
 			'<tr>'
-		, '<td><i class="glyphicon glyphicon-thumbs-down text-danger"></i></td>'
+		, '<td><i class="glyphicon glyphicon-thumbs-down text-danger" data-lang-tkey="state-title" title="Port tcp 3389 (RDP) reachable?"></i></td>'
 		, '<td>', mac, '</td>'
 		, '<td>', ip, '</td>'
 		, '<td>', cidr, '</td>'
@@ -575,19 +792,23 @@ $(function () { 'use strict'
 		$('#exportData').val(json);
 	}
 
-  var lastUpdateIndex = 1;
+  
+  var lastUpdateIndex = 0;
   function updateHostInfo() {
-		console.log()
 		var 
-		    $tr = $('#items tbody tr:nth-child(' + lastUpdateIndex + ')'),
+		    $tr = $('#items tbody tr:nth-child(' + (++lastUpdateIndex) + ')'),
 			  $i = $tr.find('td:first-child >'),
-				item = $tr.data('wol') || {},
+				item = $tr.data('wol') || { },
 				url= '?op=info&ip=' + item.ip
 				;
 
-    // Now table row found then reset index to 0
-    if(0===$tr.length) lastUpdateIndex = 1; else lastUpdateIndex++;
-
+    // No table row found then reset index to 0
+    if(0===$tr.length) {
+			lastUpdateIndex = 0; 
+			setTimeout(updateHostInfo, 100);
+			return;
+		}
+		
     // Make ajax request to get the state of the host
     $.ajax({
         url: url,
@@ -644,7 +865,8 @@ $(function () { 'use strict'
 					'wakeup': 'Aufwecken!',
 					'remove': 'Entfernen',
 					'tpl-comment': 'Mein Notebook',
-					'add': 'Hinzufügen'
+					'add': 'Hinzufügen',
+					'state-title': 'Ist TCP Port 3389 (RDP) zu erreichen?'
         },
         'en-US': {
 					'wake-on-lan': 'Wake On LAN',
@@ -658,7 +880,8 @@ $(function () { 'use strict'
 					'wakeup': 'Wake Up!',
 					'remove': 'Remove',
 					'tpl-comment': 'my notebook',
-					'add': 'Add'
+					'add': 'Add',
+					'state-title': 'Port tcp 3389 (RDP) reachable?'
         }
       }
   });
@@ -700,7 +923,7 @@ $(function () { 'use strict'
     $.ajax({
         url: url,
        type: 'GET',
-       data: { op:'wol', mac: item.mac, ip: item.ip, cidr: item.cidr, port: item.port},
+       data: { debug: debug, op:'wol', mac: item.mac, ip: item.ip, cidr: item.cidr, port: item.port},
        beforeSend: function(/* xhr */) { 
 			  },
        success:  function(resp) {
